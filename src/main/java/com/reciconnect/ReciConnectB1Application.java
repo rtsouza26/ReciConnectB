8ff6fc4e8c05c9785aa9be45198dc97475978db9
rtsouza26 <rtsouza26@gmail.com>
2019-05-25 16:24:20 -0300

    Commit inicial

---
+++ src/main/java/com/reciconnect/ReciConnectB1Application.java
@@ -0,0 +1,13 @@
+package com.reciconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReciConnectB1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReciConnectB1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/reciconnect/ReciConnectB1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reciconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReciConnectB1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
